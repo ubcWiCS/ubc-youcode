c75563f2aa1c6324091c36b1641738d2506854dc
Beta-test-initiation <85096070+Beta-test-initiation@users.noreply.github.com>
2024-01-07 19:41:40 -0800

    updated about section and stats

--- components/TimeLine.jsx
+++ components/TimeLine.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Image from 'next/image'
 
 const TimeLine = () => {
   return (
-    <div>TimeLine</div>
+    <div className='flex justify-end mr-20'>
+      <Image src="/assets/timeline.png" height={1050} width={850}  alt="timeline" />
+    </div>
   )
 }
 
