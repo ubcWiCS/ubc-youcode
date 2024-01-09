e6991c96bbf02d05fdd89bf433e5bb618aff03ee
Beta-test-initiation <85096070+Beta-test-initiation@users.noreply.github.com>
2024-01-08 22:58:55 -0800

    added moving elements

---
+++ components/Countdown.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Countdown = () => {
+  return (
+    <div className='flex flex-col items-center justify-center w-1/2'>
+      Countdown
+    </div>
+  )
+}
+
+export default Countdown
--- components/sponsors.jsx
+++ components/sponsors.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Sponsors = () => {
+  
   return (
-    <div>Sponsors</div>
+
+    <div className="flex p-20 ">
+      <p className='text-5xl font-bold text-white'> 
+        Our Sponsors: 
+      </p>
+      <div className="flex mx-20">
+        <Image src="/assets/arcteryx.png" height={200} width={200}  />
+      </div>
+      
+    </div>
   )
 }
 
