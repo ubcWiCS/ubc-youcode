ee377789dc7cc4696dfacfab66e105d94aaae4e6
Chohyeon kim <chohyeonkim94@gmail.com>
2024-01-31 20:13:17 -0800

    Change Stats: Numbers bold and text unbold

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -20,11 +20,15 @@ const Stats = () => {
         <div className="flex"> 
           <div className="mx-10 w-1/2 relative" ref={cloud1Ref} style={cloud1Style}>
             <Image src="/assets/cloud3.svg" height={150} width={350} layout="responsive" alt="cloud" />
-            <p className='text-3xl font-bold text-dark-green absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 font-bold '>100+ Participants</p>
+            <p className='text-3xl text-dark-green absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 '>
+              <span className="font-bold">100+</span> Participants
+            </p>
           </div>
           <div className="mx-10 w-1/2 relative" ref={cloud2Ref} style={cloud2Style}>
             <Image src="/assets/cloud4.svg" height={150} width={300} layout="responsive" alt="cloud" />
-            <p className='text-3xl font-bold text-dark-green absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10'>30+ Projects</p>
+            <p className='text-3xl text-dark-green absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10'>
+              <span className="font-bold">30+</span> Projects
+            </p>
           </div>
         </div>
         <div className="flex">
