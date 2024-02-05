9974e269fe885b6f469c107fc4a7f23d1761baa3
Beta-test-initiation <85096070+Beta-test-initiation@users.noreply.github.com>
2024-02-04 21:49:52 -0800

    testing new logo

--- components/Register.jsx
+++ components/Register.jsx
@@ -6,22 +6,22 @@ const Register = () => {
   return (
     <div className='flex flex-col items-center justify-center w-1/2 md:ml-20 lg:ml-0 md:mt-10 lg:mt-0'>
 
-      <p className='text-[7px] md:text-[15px] lg:text-lg  text-white font-bold  drop-shadow-md'>WiDS@UBC x UBC WiCS</p>
-      <div className=" md:ml-5 w-36 h-12 md:w-60 md:h-20 lg:w-96 lg:h-28 relative ">
+      
+      <div className=" md:ml-5 w-36 h-12 md:w-60 md:h-20 lg:w-96 lg:h-48 relative ">
             <Link href="/" className="ml-5 hover:scale-105 transition duration-500">
-              <Image src="/assets/youCode.png" fill
+              <Image src="/assets/youCodeLogo.png" fill
                     alt="YouCode Logo"   />
             </Link>
 
         </div>
-
-      <p className='text-[7px] md:text-lg lg:text-xl text-dark-green font-bold lg:mt-5 drop-shadow-md'>24-hour wellness-themed hackathon</p>
+      <p className='text-[7px] md:text-[15px] lg:text-lg  text-dark-green font-bold  drop-shadow-md'>WiDS@UBC x UBC WiCS</p>
+      <p className='text-[7px] md:text-lg tracking-wider lg:text-xl text-dark-green font-bold  drop-shadow-md'>24-hour wellness-themed hackathon</p>
       <p className='text-[7px] md:text-lg lg:text-xl text-dark-green font-bold  drop-shadow-md'>April 5-6, 2024</p>
       <Link href="/" className='mt-2 lg:mt-8'>
         <p className='hidden text-[12px]  md:text-xl lg:text-2xl bg-[#B72E38] border-[#C7444D]/[1] px-7 rounded-3xl py-1 transform hover:scale-105 transition duration-500 drop-shadow-xl '>
           Apply!
         </p>
-        <p className='text-[8px]  md:text-xl lg:text-2xl  px-7 rounded-3xl py-1 transform hover:scale-105 transition duration-500 drop-shadow-xl '>
+        <p className='text-[8px] tracking-wider md:text-xl lg:text-2xl  px-7 rounded-3xl py-1 transform hover:scale-105 transition duration-500 drop-shadow-xl '>
           Registration opens in March!
         </p>
       </Link>
