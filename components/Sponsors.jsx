07a2bb2b72e7f68d917c2b5d4747ac3ea7ecf7f4
Beta-test-initiation <85096070+Beta-test-initiation@users.noreply.github.com>
2024-02-08 20:43:42 -0800

    feedback incorporated on design, added sponsors

--- components/Sponsors.jsx
+++ components/Sponsors.jsx
@@ -1,25 +1,25 @@
 import React from 'react'
 import Image from 'next/image'
-import { useParallax } from 'react-scroll-parallax';
+
 
 const Sponsors = () => {
   
-  const { ref: cloud1Ref, style: cloud1Style } = useParallax
-    ({
-      easing: 'easeOutQuad',
-    translateX: ['-2vw', '4vw'],
-    translateY: ['-1vw', '1vw'], // Adjust the range to control the floating effect
-    scale: [1, 1],
-  });
+  
 
   return (
-    <div className="ml-8 mt-4 flex flex-col md:mt-40 " id="sponsors">
-      <p className='text-lg md:text-3xl lg:text-5xl font-bold text-white drop-shadow-[0_0_4px_#E2D6FF]'> 
+    <div className=" mt-4 flex flex-col md:mt-40  " id="sponsors">
+      <p className='text-lg md:text-3xl lg:text-5xl font-bold text-white ml-8 drop-shadow-[0_0_4px_#E2D6FF]'> 
         Our Sponsors 
       </p>
-      <div className="mt-10 md:mt-20 lg:mt-40"></div>
-      <div className="flex w-10 h-10  md:w-32 md:h-32 lg:w-60 lg:h-60 mx-20 relative" ref={cloud1Ref} style={cloud1Style}>
+      <div className="flex flex-col items-center justify-center mt-4 md:mt-10 lg:-28 rounded-2xl bg-white/[0.1] p-3 md:p-10 lg:p-20 mx-8 md:mx-20">
+      <div className="flex w-20 h-10  md:w-32 md:h-32 lg:w-5/12 lg:h-48  relative">
         <Image src="/assets/arcteryx.png" fill  />
+      </div>
+      <div className="flex w-full mt-4 md:mt-10 lg:mt-28">
+        <div className="flex w-20 h-10  md:w-32 md:h-32 lg:w-4/12 lg:h-24 relative">
+          <Image src="/assets/schneider.png" fill  />
+        </div>
+      </div>
       </div>
       
     </div>
