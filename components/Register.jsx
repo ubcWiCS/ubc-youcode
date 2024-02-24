756ae6f460002af2bb29379dee9a5d8d07c4d18c
Tanya-movmint <tanya@movmint.io>
2024-02-23 18:18:01 -0800

    date change

--- components/Register.jsx
+++ components/Register.jsx
@@ -16,7 +16,7 @@ const Register = () => {
         </div>
       <p className='text-[7px] md:text-[15px] tracking-widest  lg:text-lg  text-white font-bold  drop-shadow-md'>WiDS@UBC & UBC WiCS</p>
       <p className='text-[7px] md:text-lg tracking-widest lg:text-xl text-white font-bold  drop-shadow-md'>24-hour wellness-themed hackathon</p>
-      <p className='text-[7px] md:text-lg lg:text-xl tracking-widest text-white  '>April 5-6, 2024</p>
+      <p className='text-[7px] md:text-lg lg:text-xl tracking-widest text-white  '>April 6-7, 2024</p>
       <Link href="/" className='mt-2 lg:mt-8'>
         <p className='hidden text-[12px]  md:text-xl lg:text-2xl bg-[#B72E38] border-[#C7444D]/[1] px-7 rounded-3xl py-1 transform hover:scale-105 transition duration-500 drop-shadow-xl '>
           Apply!
